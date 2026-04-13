122ae7cb452aeaff30265c8aa18c5cb6790c8aac
Francesco Martellosio <francesco.martellosio@mail.polimi.it>
2026-04-13 10:56:27 +0200

    small fix inside init.py

--- nitric/resources/__init__.py
+++ nitric/resources/__init__.py
@@ -24,7 +24,7 @@ from nitric.resources.kv import KeyValueStoreRef, kv
 from nitric.resources.schedules import ScheduleServer, schedule
 from nitric.resources.secrets import Secret, secret
 from nitric.resources.spark import Spark, spark
-from nitric.resources.analyticsservice import AnalyticsService, analytics_service
+from nitric.resources.analytics import AnalyticsService, analytics_service
 from nitric.resources.topics import Topic, topic
 from nitric.resources.websockets import Websocket, websocket
 from nitric.resources.queues import Queue, queue
